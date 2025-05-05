7f36a4cf42396292c39a37020a5850f4daea148e
Sarffk17 <Ksarff17@gmail.com>
2025-05-04 21:05:46 -0400

    Created footer

--- src/App.js
+++ src/App.js
@@ -3,9 +3,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Dictionary
-      </header>
+      <div className="container">
+        <header className="App-header">
+          Dictionary
+        </header>
+        <footer>
+        This dictonary project is coded by 
+        <a 
+        href="https://github.com/Sarffk17" 
+        target="_blank">
+        Kelly Sarff 
+        </a>
+        and is open-sourced on 
+        <a 
+        href="https://github.com/Sarffk17/dictionary-define" 
+        target="_blank">
+        GithUb </a>
+        and is hosted on
+        <a 
+        href="https://dictionary-define.netlify.app/" 
+        target="_blank">
+        Netlify </a>
+        </footer>
+      </div>
     </div>
   );
 }
